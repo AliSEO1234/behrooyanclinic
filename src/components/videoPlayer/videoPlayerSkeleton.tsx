b6e2e9c61aed8df59b6b9960b1c38c431a5992e0
amirdavari-dev <devamirdavari@gmail.com>
2025-05-29 14:43:55 +0330

    videoplayer fixed

---
+++ src/components/videoPlayer/videoPlayerSkeleton.tsx
@@ -0,0 +1,8 @@
+const VideoPlayerSkeleton = () => (
+    <div className="mb-8">
+      <div className="aspect-video w-full bg-gray-200 2xl:rounded-[50px] 4xl:rounded-[55px] 8xl:rounded-[60px] fhd:rounded-[65px] animate-pulse" />
+    </div>
+  );
+  
+  export default VideoPlayerSkeleton;
+  
--- src/features/videoPlayer/videoPlayer.tsx
+++ src/features/videoPlayer/videoPlayer.tsx
@@ -1,20 +1,25 @@
 "use client";
-
-import { Skeleton } from "@/components/ui/skeleton";
+import VideoPlayerSkeleton from "@/components/videoPlayer/videoPlayerSkeleton";
 import { Suspense } from "react";
-
+import ReactPlayer from "react-player";
 const VideoPlayerCom = () => {
   return (
     <div className="video-header">
-      <Suspense
-      fallback={<Skeleton className="w-full h-full" />}
-      >
-        <video
+      <Suspense fallback={<VideoPlayerSkeleton />}>
+        {/* <video
           autoPlay
           muted
           loop={true}
           playsInline
           src="/videos/videohealth1.mp4"
+        /> */}
+        <ReactPlayer
+          url={`/videos/videohealth1.mp4`}
+          playing
+          muted
+          loop
+          width="100%"
+          height="100%"
         />
       </Suspense>
     </div>
