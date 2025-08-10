0f89bb7aca80e13252c422a891e100401c94a415
Hossein Salari <salaryh801@gmail.com>
2025-08-10 16:10:23 +0330

    fix main page services scroll

--- src/components/services/scrollServices.tsx
+++ src/components/services/scrollServices.tsx
@@ -2,31 +2,36 @@
 import ServiceCard from "@/features/services/serviceCard";
 import handleServices from "@/staticData/services/handleServices";
 import { useLocale } from "next-intl";
-import { useEffect, useRef } from "react";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation, Pagination } from "swiper/modules";
+import "./serviceStyles.css"
 
 const ScrollServices = () => {
   const locale = useLocale()
   const services = handleServices(locale) || [];
-  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    if (scrollContainerRef.current) {
-      const container = scrollContainerRef.current;
-      const thirdCard = container.children[2] as HTMLDivElement;
-      if (thirdCard) {
-        const offset = thirdCard.offsetLeft - container.offsetWidth / 2 + thirdCard.offsetWidth / 2;
-        container.scrollLeft = offset;
-      }
-    }
-  }, []);
   return (
     <div className={"flex xl:justify-center"}>
-    <div ref={scrollContainerRef}
-         className="flex flex-nowrap overflow-x-auto gap-5 2xl:gap-8 px-2 items-center justify-start gap-y-8 scroll-smooth snap-x snap-mandatory pb-4">
-      {services.map(({ countService, img, label, path }, index) => {
+      <Swiper
+        modules={[Navigation, Pagination, FreeMode]}
+        spaceBetween={16}
+        slidesPerView={"auto"}
+        freeMode={true}
+        pagination={{ clickable: true, }}
+        navigation={true}
+        className="w-full !pb-3 !px-3"
+      >
+        {services.map(({ countService, img, label, path }, index) => {
         return (
-          <ServiceCard
+          <SwiperSlide
             key={index}
+            className="!w-[210px] !min-w-[210px]
+                        s1512:!w-[252px] s1512:!min-w-[252px]
+                        s1600:!w-[262px] s1600:!min-w-[262px]
+                        s1728:!w-[282px] s1728:!min-w-[282px]
+                        snap-center me16">
+          <ServiceCard
             countService={countService}
             img={img}
             title={label}
@@ -34,11 +39,12 @@ const ScrollServices = () => {
             linkStyle=" text-[18px] s1512:text-[18px]"
             titleStyle={`${locale=== "ru" ? "text-[14px] s1512:text-[16px]" : "text-[20px] s1512:text-[20px]"}`}
             imgStyle="p-4 w-[190px] h-[190px] s1280:h-[180px] s1280:w-[180px] s1512:h-[220px] s1512:w-[220px] s1728:w-[250px] s1728:h-[250px]"
-            className="treatments__card-container w-[210px] min-w-[210px] s1512:w-[252px] s1512:min-w-[252px] s1600:w-[262px]  s1600:min-w-[262px] s1728:w-[282px] s1728:min-w-[282px] snap-center"
+            className=" w-full"
           />
+          </SwiperSlide>
         );
       })}
-    </div>
+      </Swiper>
     </div>
   );
 };
