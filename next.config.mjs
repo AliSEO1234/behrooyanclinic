8ea2f9830fdc730f78ac568f2355f4ea68943f53
AliSEO1234 <Ali@realreach.eu>
2026-03-16 13:18:29 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,27 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
